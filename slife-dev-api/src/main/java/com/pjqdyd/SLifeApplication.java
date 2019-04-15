29b4d4f860ce8e3cd20810b9fb2aad5e69158c1b
pjqdyd <1909025079@qq.com>
2019-04-15 19:10:37 +0800

    --init--

---
+++ slife-dev-api/src/main/java/com/pjqdyd/SLifeApplication.java
@@ -0,0 +1,16 @@
+package com.pjqdyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Springboot启动类
+ */
+@SpringBootApplication
+public class SLifeApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SLifeApplication.class, args);
+    }
+
+}

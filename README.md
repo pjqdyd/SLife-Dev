e50396aac7ad20076581b6430e19999360bd4a22
pjqdyd <1909025079@qq.com>
2019-04-22 21:49:07 +0800

    --获取附近的店铺接口完成--

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## _![](https://upload-images.jianshu.io/upload_images/14511997-ac105e36e219f00e.png)_ SLife-Dev [生活街App](https://github.com/pjqdyd/UniApp-SLife)的后端项目 (SpringBoot 2.1.4.RELEASE)
 
-(项目初始化...)
+(项目初始化,文档后续更新...)
 
--- slife-dev-dao/src/main/java/com/pjqdyd/dao/ShopRepository.java
+++ slife-dev-dao/src/main/java/com/pjqdyd/dao/ShopRepository.java
@@ -1,6 +1,8 @@
 package com.pjqdyd.dao;
 
 import com.pjqdyd.pojo.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**   
@@ -14,5 +16,10 @@ public interface ShopRepository extends JpaRepository<Shop, String> {
 
     //通过店铺id查询简要信息的方法
     Shop findByShopId(String shopId);
+
+    //根据用户的经度区间and纬度区间查询附近的店铺, 并按评分排序
+    Page<Shop> findAllByShopLatitudeBetweenAndShopLongitudeBetweenAndShopStatusEquals(
+            Double minLat, Double maxLat,  Double minLot, Double maxLot, Integer shopStatus, Pageable pageable);
+
 
 }

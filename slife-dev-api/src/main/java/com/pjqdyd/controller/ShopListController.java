b1581dd261e001bad680b0b23f2d84f4ebdc3bc0
pjqdyd <1909025079@qq.com>
2019-04-30 16:32:59 +0800

    --change--

--- slife-dev-api/src/main/java/com/pjqdyd/controller/ShopListController.java
+++ slife-dev-api/src/main/java/com/pjqdyd/controller/ShopListController.java
@@ -25,7 +25,7 @@ import java.util.List;
  *  
  */
 @Slf4j
-@Api(value = "店铺列表Controller层", tags = "查询店铺列表")
+@Api(value = "店铺列表相关接口", tags = "店铺列表Controller层")
 @CrossOrigin
 @RestController
 @RequestMapping("/slife/shopList")
@@ -38,7 +38,7 @@ public class ShopListController {
     private ShopListService shopListService;
 
     /**
-     * 获取附近的商铺
+     * 获取附近的商铺,分页
      *
      * @param latitude  用户所在位置的纬度
      * @param longitude 用户所在位置的经度
@@ -46,7 +46,7 @@ public class ShopListController {
      * @param size      当前要查询的数据条数
      * @return 包含
      */
-    @ApiOperation(value = "查询附近的商铺", tags = "主页查询附近商铺")
+    @ApiOperation(value = "分页查询附近的商铺", tags = "主页查询附近商铺")
     @GetMapping("/localShop")
     public ResponseResult getLocalShopList(@RequestParam("latitude") Double latitude,
                                            @RequestParam("longitude") Double longitude,
@@ -76,6 +76,13 @@ public class ShopListController {
     }
 
 
+    /**
+     * 根据分类模糊查询附近的店铺
+     * @param latitude 用户纬度
+     * @param longitude 用户经度
+     * @param category 分类名
+     * @return
+     */
     @ApiOperation(value = "查询附近的分类商铺", tags = "根据分类查询附近的商铺")
     @GetMapping("/localCateShop")
     public ResponseResult getLocalCateShopList(@RequestParam("latitude") Double latitude,
@@ -86,6 +93,23 @@ public class ShopListController {
         List<Shop> shopList = shopListService.findAllLocalAndLikeCategory(latitude, longitude, category, 1);
 
         return ResponseResult.success(shopList);
+    }
+
+    /**
+     * 查询附近所有的店铺,不分类,不分页
+     * @param latitude
+     * @param longitude
+     * @return
+     */
+    @ApiOperation(value = "查询附近所有的商铺", tags = "查询附近所有商铺")
+    @GetMapping("/allLocalShop")
+    public ResponseResult getAllLocalShopList(@RequestParam("latitude") Double latitude,
+                                              @RequestParam("longitude") Double longitude){
+        //查询附近的所有店铺
+        List<Shop> shopList = shopListService.findAllLocaly(latitude, longitude, 1);
+
+        return ResponseResult.success(shopList);
+
     }
 
 }

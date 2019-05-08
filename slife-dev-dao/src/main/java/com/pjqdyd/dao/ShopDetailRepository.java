8fc3a3f6c83f906dcf06ea560826a75186dd09c1
pjqdyd <1909025079@qq.com>
2019-05-08 15:06:36 +0800

    --change--

--- slife-dev-dao/src/main/java/com/pjqdyd/dao/ShopDetailRepository.java
+++ slife-dev-dao/src/main/java/com/pjqdyd/dao/ShopDetailRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 /**   
  * @Description:  [店铺详情repository接口]
